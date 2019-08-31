ad72caae3793ed95e32d47e15efb510172c60256
sbx_user1051 <sbx_user1051@169.254.12.197>
2019-08-31 17:55:45 +0000

    Sat Aug 31 17:55:45 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,32 +4,32 @@ from io import StringIO
 
 def _read_prices():
     _input = """
-    | A    | 50    | 3A for 130, 5A for 200 |
-    | B    | 30    | 2B for 45              |
-    | C    | 20    |                        |
-    | D    | 15    |                        |
-    | E    | 40    | 2E get one B free      |
-    | F    | 10    | 2F get one F free      |
-    | G    | 20    |                        |
-    | H    | 10    | 5H for 45, 10H for 80  |
-    | I    | 35    |                        |
-    | J    | 60    |                        |
-    | K    | 80    | 2K for 150             |
-    | L    | 90    |                        |
-    | M    | 15    |                        |
-    | N    | 40    | 3N get one M free      |
-    | O    | 10    |                        |
-    | P    | 50    | 5P for 200             |
-    | Q    | 30    | 3Q for 80              |
-    | R    | 50    | 3R get one Q free      |
-    | S    | 30    |                        |
-    | T    | 20    |                        |
-    | U    | 40    | 3U get one U free      |
-    | V    | 50    | 2V for 90, 3V for 130  |
-    | W    | 20    |                        |
-    | X    | 90    |                        |
-    | Y    | 10    |                        |
-    | Z    | 50    |                        |
+    | A    | 50    | 3A for 130, 5A for 200          |
+    | B    | 30    | 2B for 45                       |
+    | C    | 20    |                                 |
+    | D    | 15    |                                 |
+    | E    | 40    | 2E get one B free               |
+    | F    | 10    | 2F get one F free               |
+    | G    | 20    |                                 |
+    | H    | 10    | 5H for 45, 10H for 80           |
+    | I    | 35    |                                 |
+    | J    | 60    |                                 |
+    | K    | 70    | 2K for 120                      |
+    | L    | 90    |                                 |
+    | M    | 15    |                                 |
+    | N    | 40    | 3N get one M free               |
+    | O    | 10    |                                 |
+    | P    | 50    | 5P for 200                      |
+    | Q    | 30    | 3Q for 80                       |
+    | R    | 50    | 3R get one Q free               |
+    | S    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+    | T    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+    | U    | 40    | 3U get one U free               |
+    | V    | 50    | 2V for 90, 3V for 130           |
+    | W    | 20    |                                 |
+    | X    | 17    | buy any 3 of (S,T,X,Y,Z) for 45 |
+    | Y    | 20    | buy any 3 of (S,T,X,Y,Z) for 45 |
+    | Z    | 21    | buy any 3 of (S,T,X,Y,Z) for 45 |
     """
     f = StringIO(_input)
     input_data = {}
@@ -74,8 +74,6 @@ def _process_free_special(counter, sku, special_str):
     x = parts[0].strip()
     y = parts[3].strip()
 
-    # print(x)
-    # print(y)
     assert len(x) == 2
     assert len(y) == 1
 
@@ -116,13 +114,16 @@ def checkout(skus: str) -> int:
     free_specials = {}
     large_specials = {}
     small_specials = {}
+    buy_any_specials = {}
 
-    # Organise specials
+    # Organise and process specials (text)
     for sku in input_data:
         special_str = input_data[sku]['specials'].strip()
         if special_str:
             if special_str.endswith('free'):
                 free_specials[sku] = special_str
+            elif special_str.startswith('buy any'):
+                buy_any_specials[sku] = special_str
             elif ',' in special_str:
                 small_spec, large_spec = special_str.split(',')
                 assert 'for' in large_spec
@@ -155,3 +156,4 @@ def checkout(skus: str) -> int:
         total += counter[sku] * int(input_data[sku]['price'])
 
     return total
+
