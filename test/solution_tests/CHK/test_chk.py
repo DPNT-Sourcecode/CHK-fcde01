1927517f0373bad4d495267bf88e3bc0b68a6a66
sbx_user1051 <sbx_user1051@169.254.81.21>
2019-08-31 14:47:17 +0000

    Sat Aug 31 14:47:17 UTC 2019

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -15,20 +15,13 @@ class TestCheckout:
     def test_checkout_1(self):
         assert checkout_solution.checkout("ABC") == 50+30+20
 
-    def test_checkout_3(self):
-        assert checkout_solution.checkout("BB") == 45
 
-    def test_checkout_5(self):
-        assert checkout_solution.checkout("BBB") == 75
 
     def test_checkout_6(self):
         assert checkout_solution.checkout("ABCD") == 115
 
     def test_checkout_7(self):
         assert checkout_solution.checkout("AAAAAAAAAAA") == 490
-
-    def test_checkout_8(self):
-        assert checkout_solution.checkout("BBBB") == 90
 
 
 class TestCheckoutA:
@@ -41,5 +34,29 @@ class TestCheckoutA:
 
     def test_checkout_5A(self):
         assert checkout_solution.checkout("AAAAA") == 180
+
+
+class TestCheckoutB:
+
+    def test_checkout_2B(self):
+        assert checkout_solution.checkout("BB") == 45
+
+    def test_checkout_3B(self):
+        assert checkout_solution.checkout("BBB") == 75
+
+    def test_checkout_4B(self):
+        assert checkout_solution.checkout("BBBB") == 90
+
+
+class TestCheckoutE:
+
+    def test_checkout_1E(self):
+        assert checkout_solution.checkout("E") == 40
+
+    def test_checkout_2E(self):
+        assert checkout_solution.checkout("EE") == 80
+
+    def test_checkout_2E1B(self):
+        assert checkout_solution.checkout("EEB") == 80
 
 
