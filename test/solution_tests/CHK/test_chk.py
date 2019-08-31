28345ca477edd56fbc5326b87567f21b8cc8191a
sbx_user1051 <sbx_user1051@169.254.81.21>
2019-08-31 14:30:38 +0000

    Sat Aug 31 14:30:38 UTC 2019

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -4,10 +4,27 @@ from solutions.CHK import checkout_solution
 class TestCheckout:
 
     def test_checkout_1(self):
-        # assert checkout_solution.checkout(1, 2) == 3
-        pass
+        assert checkout_solution.checkout("ABC") == 50+30+20
 
     def test_checkout_2(self):
-        # assert checkout_solution.checkout(100, 100) == 200
-        pass
+        assert checkout_solution.checkout("AAA") == 130
+
+    def test_checkout_3(self):
+        assert checkout_solution.checkout("BB") == 45
+
+    def test_checkout_4(self):
+        assert checkout_solution.checkout("AAAA") == 180
+
+    def test_checkout_5(self):
+        assert checkout_solution.checkout("BBB") == 75
+
+    def test_checkout_6(self):
+        assert checkout_solution.checkout("ABCD") == 115
+
+    def test_checkout_7(self):
+        assert checkout_solution.checkout("AAAAAAAAAAA") == 490
+
+    def test_checkout_8(self):
+        assert checkout_solution.checkout("BBBB") == 90
+
 
