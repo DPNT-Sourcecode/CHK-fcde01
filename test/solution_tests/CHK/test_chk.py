97a421bc4f1e5d588c9c3204757b34577299120a
sbx_user1051 <sbx_user1051@169.254.81.21>
2019-08-31 14:18:21 +0000

    Sat Aug 31 14:18:21 UTC 2019

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -1,12 +1,13 @@
 from solutions.CHK import checkout_solution
 
 
-class TestSum:
+class TestCheckout:
 
-    def test_sum_12(self):
+    def test_checkout_1(self):
         # assert checkout_solution.checkout(1, 2) == 3
         pass
 
-    def test_sum_100100(self):
+    def test_checkout_2(self):
         # assert checkout_solution.checkout(100, 100) == 200
         pass
+
