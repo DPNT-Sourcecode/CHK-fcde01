2bc278bfba0472e60fbdbdef9060f06ce503a121
sbx_user1051 <sbx_user1051@169.254.81.21>
2019-08-31 14:36:38 +0000

    Sat Aug 31 14:36:38 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,8 +3,22 @@ from collections import Counter
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str) -> int:
-    c = Counter(skus)
-    
+    if not isinstance(skus, str):
+        return -1
+
+    c = Counter(skus.lower())
+    a_count = c['a']
+    b_count = c['b']
+    c_count = c['c']
+    d_count = c['d']
+
+    a_special = a_count // 3
+    a_count = a_count % 3
+    b_special = b_count // 2
+    b_count = b_count % 2
+
+    return (a_special * 130) + (a_count * 50) + (b_special * 45) + (b_count * 30) + (c_count * 20) + (d_count * 15)
+
 
 
 
